da46f164d93b6b4939d7e892908f853a6159bfb4
sbcodes90 <sydneycburrell29@gmail.com>
2024-06-06 21:27:43 -0400

    landing page creation

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -32,8 +32,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <>
-
   <Outlet />
-  <Navigation />
   </>;
 }
--- my-remix-app/app/routes/_index.tsx
+++ my-remix-app/app/routes/_index.tsx
@@ -1,21 +1,22 @@
 import type { MetaFunction } from "@remix-run/node";
-import logo from "../images/header.png";
-
+import logo from "../images/new-header.png";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "123AllEyesOnMe LLC" },
+    { name: "description", content: "Tutoring service for kids and parents" },
   ];
 };
 
 export default function Index() {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <img src={logo} alt="logo" className="w-full" />
-    
+      <div className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-400 to-teal-200 min-h-screen">
+        <img src={logo} alt="logo" />
+        <button className="bg-yellow-400 px-16 py-5 rounded-3xl font-black -translate-y-10"><Link to="Home">Enter the hive</Link></button>
       </div>
+    
     </>
   );
 }
