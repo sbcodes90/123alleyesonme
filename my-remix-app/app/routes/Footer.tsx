f41d90efc16f47b55708049f4205c92990c6c5da
sbcodes90 <sydneycburrell29@gmail.com>
2024-06-13 14:58:47 -0400

    footer update

--- my-remix-app/app/routes/Footer.tsx
+++ my-remix-app/app/routes/Footer.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-black text-white p-96'>
+    <div className='bg-black text-white h-[400px] pt-10 lg:p-96 lg:pt-20'>
     <div className='text-center text-2xl font-bold'>Footer</div>
     </div>
 
